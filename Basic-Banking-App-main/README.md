7a083b86d98e0c958d656d4800a421cb706cb46c
akasharm1999 <88958901+akasharm1999@users.noreply.github.com>
2022-09-17 12:33:18 +0530

    Add files via upload

---
+++ Basic-Banking-App-main/README.md
@@ -0,0 +1,2 @@
+I have created this basic banking app repository for sparks foundation intership. Its implementation video URL is as follows
+https://youtu.be/gD0Suxqx1yI
---
+++ Basic-Banking-App-main/app/src/main/java/com/example/myapplication5/TransferActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.myapplication5.MainActivity;
+
+public class TransferActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+
+        Button button = findViewById(R.id.button4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(TransferActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                Toast.makeText(TransferActivity.this, "Successfully Sent!!!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
